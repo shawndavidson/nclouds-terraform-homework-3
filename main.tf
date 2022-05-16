24b5e0fd57b2fc709ceb7577b10970712a2c72ab
Shawn Davidson <shawnkdavidson@gmail.com>
2022-05-16 14:22:49 -0700

    Add launch config and auto scaling group modules

--- main.tf
+++ main.tf
@@ -7,6 +7,23 @@ locals {
 module "vpc" {
   source = "./modules/vpc"
 
-  owner   = "sdavidson"
-  project = "sdavidson-terraform"
+  owner   = "${var.owner}"
+  project = "${var.project}"
+}
+
+module "launch_config_module" {
+  source = "./modules/launch-config"
+
+  owner   = "${var.owner}"
+  project = "${var.project}"
+}
+
+module "aws_auto_scaling_group" {
+  source = "./modules/auto-scaling-group"
+  
+  owner   = "${var.owner}"
+  project = "${var.project}"
+
+  launch_configuration  = module.launch_config_module.name
+  subnet_ids            = module.vpc.subnet_ids
 }
